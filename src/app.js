d5897e40c7aa18184d9b46e009479a2148d2ce8c
vini <vinicius.brandaot@gmail.com>
2023-01-09 14:33:25 -0300

    post tweet

--- src/app.js
+++ src/app.js
@@ -7,15 +7,11 @@ app.use(cors());
 app.use(express.json());
 
 const users = [];
-const tweets = [{
-    username: "bobesponja",
-    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-    tweet: "eu amo o hub"
-}];
+const tweets = [];
 
 app.post("/sign-up", (req, res) => {
     const user = req.body
-    if(!(user.username && user.avatar)){
+    if(!user.username || !user.avatar){
         return res.status(400).send("Todos os campos são obrigatórios!");
     }    
     users.push(user)
@@ -23,17 +19,27 @@ app.post("/sign-up", (req, res) => {
 })
 
 app.get("/tweets", (req, res) =>{
-    const tweetFeed = tweets.reverse(Math.max(tweets.length -10, 0))
-    res.send(tweets.slice());
+    const tweetFeed = tweets.slice(Math.max(tweets.length -10, 0))
+    res.send(tweetFeed.reverse());
 })
 
 
 
 app.post("/tweets", (req, res) => {
-    const tweet = req.body
-    tweets.push(tweet)
-    res.send(tweet)
+    const postedTweet = req.body
+    const user = users.find(u => u.username === postedTweet.username)
+
+    if(!postedTweet.username || !postedTweet.tweet){
+        return res.status(400).send("BAD REQUEST")
+    }
+    if (!user){
+        return res.status(401).send("UNAUTHORIZED")
+    }
+
+    tweets.push({... postedTweet, avatar: user.avatar})
+    res.status(201).send(postedTweet)
 })
+
 
 
 app.listen(5000, () => console.log('Servidor conectado'));
