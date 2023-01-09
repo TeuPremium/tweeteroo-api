3b21289fb7fe3fd55e5cc8c5cfc0f9d48b3d7c96
vini <vinicius.brandaot@gmail.com>
2023-01-09 10:53:43 -0300

    installing packeges

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log("ola mundo")
+import express from "express";
+
+const app = express()
+
+app.get("/", (req, res) =>{
+
+    res.send('jorge');
+})
+
+app.listen(5000)
+console.log("ola mundo");
