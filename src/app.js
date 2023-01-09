792164e8680049861e9eacaaf1e9b33b511f1cc8
vini <vinicius.brandaot@gmail.com>
2023-01-09 13:37:30 -0300

    user enter

--- src/app.js
+++ src/app.js
@@ -1,35 +1,29 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 
 
-const app = express()
-app.use(express.json)
-app.use(cors())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-
-const users = []
-const tweets = []
-
-
-app.get("/", (req, res) =>{
-
-    res.send('jorge');
-})
-
-app.get("tweets", (req, res) =>{
-
-    res.send(tweets.slice(tweets.length -10));
-})
+const users = [];
+const tweets = [];
 
 app.post("/sign-up", (req, res) => {
     const user = req.body
-    if(user.username && user.avatar){users.push(user)
-    }
-    else{
-        return res.status(400).send("Os campos devem estar todos preenchidos");
-    }
+    if(!(user.username && user.avatar)){
+        return res.status(400).send("Todos os campos são obrigatórios!");
+    }    
+    users.push(user)
     res.status(201).send('OK')
 })
+
+
+app.get("tweets", (req, res) =>{
+
+    res.send(tweets.slice(tweets.length -10, 0));
+})
+
 
 app.post("/tweets", (req, res) => {
     const tweet = req.body
@@ -37,4 +31,5 @@ app.post("/tweets", (req, res) => {
     res.send(tweet)
 })
 
-app.listen(5000, () => {console.log('server is connected')});
+
+app.listen(5000, () => console.log('Servidor conectado'));
