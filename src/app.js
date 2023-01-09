bedc48c0f600c5a0cc3ef77282334b78bd15cb97
vini <vinicius.brandaot@gmail.com>
2023-01-09 12:53:08 -0300

    routes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
 import express from "express";
+import cors from "cors";
+
 
 const app = express()
+app.use(express.json)
+app.use(cors())
+
+
+const users = []
+const tweets = []
+
 
 app.get("/", (req, res) =>{
 
     res.send('jorge');
 })
 
-app.listen(5000)
-console.log("ola mundo");
+app.get("tweets", (req, res) =>{
+
+    res.send(tweets.slice(tweets.length -10));
+})
+
+app.post("/sign-up", (req, res) => {
+    const user = req.body
+    if(user.username && user.avatar){users.push(user)
+    }
+    else{
+        return res.status(400).send("Os campos devem estar todos preenchidos");
+    }
+    res.status(201).send('OK')
+})
+
+app.post("/tweets", (req, res) => {
+    const tweet = req.body
+    tweets.push(tweet)
+    res.send(tweet)
+})
+
+app.listen(5000, () => {console.log('server is connected')});
